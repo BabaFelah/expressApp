e848c19f3edca629f1996bf74853ad4546300a9c
abdul-yesuf <messagekerim@gmail.com>
2019-10-31 21:31:38 -0500

    some cleanup

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,12 @@
     email TEXT
 );
 
+/*
+You need to create a .env file and define the database connection as follows
+    DB_HOST='localhost'
+    DB_USER=postgres
+    DB_DATABASE=express_mvp
+    DB_PASSWORD=null
+    DB_PORT=5432
+
+*/
